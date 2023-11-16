c3323e0a34b6731b386d63f73db29f5160e4d946
Lucas S <lucas.santos.silva2011@gmail.com>
2023-11-15 21:46:41 -0300

    Added routing navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Feed from './pages/Feed.jsx'
+import PostDetail from './pages/PostDetail.jsx'
+import Main from './layouts/Main.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Feed />
+      },
+      {
+        path: '/posts/:id',
+        element: <PostDetail />
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
