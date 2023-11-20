433a95d6e2690205103a680f8bcf9d1f3bf9ecaa
Lucas S <lucas.santos.silva2011@gmail.com>
2023-11-20 12:56:48 -0300

    Added example

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,23 @@
 import { Box, CardContent, Typography } from "@mui/material";
-import { Card } from "@mui/joy";
+import { Button, Card, CardActions } from "@mui/joy";
 
 import { grey } from "@mui/material/colors";
 import { Link } from "react-router-dom";
+import { Favorite } from "@mui/icons-material";
+import { useState } from "react";
 
 export default function Post({ post }) {
+  const [starsCount, setStarsCount] = useState(post.starsCount)
+
+  const favorite = async () => {
+    const response = await fetch(`http://localhost:3000/posts/${post.id}/favorite`, {
+      method: 'PUT'
+    })
+
+    const updatedPost = await response.json()
+    setStarsCount(updatedPost.starsCount)
+  }
+
   return (
     <Card sx={{ marginBottom: '1em', width: '100%' }}>
       <Link to={`/posts/${post.id}`}>
@@ -15,6 +28,11 @@ export default function Post({ post }) {
           </Typography>
         </CardContent>
       </Link>
+      <CardActions>
+        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+          <Button variant="outlined" startDecorator={<Favorite />} onClick={favorite}>{ starsCount }</Button>
+        </Box>
+      </CardActions>
     </Card>
   )
 }
